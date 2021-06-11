bc9e2219350bff5c1687c8f79e560e29f0aef744
Phil Datoon <Philip_Bryan_Datoon@manulife.ca>
2021-06-11 15:20:49 +0800

    Implement bundle splitting and cache busting

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,25 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default {
   mode: 'production',
   devtool: 'source-map',
-  entry: './src/index.js',
+  entry: {
+    main: path.resolve(__dirname, './src/index'),
+    vendor: path.resolve(__dirname, './src/vendor')
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js',
   },
   plugins: [
+    // generate an external CSS file with a hash  in the filename
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash].css'
+    }),
+
     // create HTML file that includes reference to bundled JS
     new HtmlWebpackPlugin({
       template: 'src/index.html'
@@ -19,7 +28,7 @@ export default {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
     ],
   },
 };
